a85c48ff5d02f9c09c8d688f4185568b9132e6a3
Casey Dunn <casey.dunn@yale.edu>
2018-01-27 09:29:15 -0500

    started code for multiple one wire temp sensors

--- freezer_photon/src/freezer_photon.ino
+++ freezer_photon/src/freezer_photon.ino
@@ -27,6 +27,8 @@ OneWire oneWire(D0);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature dallas(&oneWire);
 
+int onewire_device_count = 0;
+
 // Create a variable that will store the temperature value
 double temperature = 0.0;
 
@@ -58,6 +60,8 @@ void setup()
   // dallas.setResolution(9);
   dallas.begin();
 
+  onewire_device_count = dallas.getDeviceCount();
+  Particle.publish("onewire_device_count", String(onewire_device_count));
   // Serial.begin(9600);
 
 }
@@ -79,6 +83,36 @@ void loop()
   // Request temperature conversion (traditional)
   dallas.requestTemperatures();
 
+  for( int i = 0; a < onewire_device_count; i++ ) {
+    DeviceAddress deviceAddress;
+    if ( !dallas.getAddress( deviceAddress, i ) )
+    {
+        Particle.publish("warning", "address not valid");
+    }
+    char output [23];
+    sprintf(&output[0], "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", deviceAddress[0], deviceAddress[1], deviceAddress[2], deviceAddress[3], deviceAddress[4], deviceAddress[5], deviceAddress[6], deviceAddress[7]);
+    // sprintf(&output1[0], "%x", deviceAddress1);
+    Particle.publish("address_1", String(output));
+
+    float tempC = dallas.getTempCByIndex(i);
+    // convert to double
+    temperature = (double)tempC;
+
+    // Print out
+    Serial.print( "Temp in C = ");
+    Serial.print( tempC );
+
+    if (tempC > -127.0 ){
+      Blynk.virtualWrite(V5, tempC);
+      Particle.publish("temperature", String(tempC));
+    }
+    else
+    {
+      Particle.publish("DS18B20_status","disconnected");
+    }
+
+  }
+
   // sin( 23423 );
   // delay(1000);
 
@@ -94,6 +128,26 @@ void loop()
   //  Particle.publish("DS18B20_status","disconnected");
   //}
 
+  DeviceAddress deviceAddress0;
+  if ( !dallas.getAddress( deviceAddress0, 0 ) )
+  {
+      Particle.publish("warning", "address not valid");
+  }
+  char output0 [16];
+  sprintf(&output0[0], "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", deviceAddress0[0], deviceAddress0[1], deviceAddress0[2], deviceAddress0[3], deviceAddress0[4], deviceAddress0[5], deviceAddress0[6], deviceAddress0[7]);
+  //sprintf(&output0[0], "%x", deviceAddress0);
+  Particle.publish("address_0", String(output0));
+
+  DeviceAddress deviceAddress1;
+  if ( !dallas.getAddress( deviceAddress1, 1 ) )
+  {
+      Particle.publish("warning", "address not valid");
+  }
+  char output1 [4];
+  sprintf(&output1[0], "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", deviceAddress1[0], deviceAddress1[1], deviceAddress1[2], deviceAddress1[3], deviceAddress1[4], deviceAddress1[5], deviceAddress1[6], deviceAddress1[7]);
+  // sprintf(&output1[0], "%x", deviceAddress1);
+  Particle.publish("address_1", String(output1));
+
   // get the temperature in Celcius
   float tempC = dallas.getTempCByIndex(0);
   // convert to double
@@ -104,10 +158,18 @@ void loop()
   Serial.print( tempC );
 
   // Publish
-  Particle.publish("temperature", String(tempC));
   Particle.publish("alarm", String(alarm));
 
-  Blynk.virtualWrite(V5, tempC);
+  if (tempC > -127.0 ){
+    Blynk.virtualWrite(V5, tempC);
+    Particle.publish("temperature", String(tempC));
+  }
+  else
+  {
+    Particle.publish("DS18B20_status","disconnected");
+  }
+
+
   if ( alarm ){
     alarm_led.on();
   }
