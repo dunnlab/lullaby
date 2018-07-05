bab49fb98c5b6566923c63f6d60a69de589e7d64
Ben Evans <benjamin.robert.evans@gmail.com>
2018-07-05 14:09:32 -0400

    copypasta error fixed

--- docker/run_lullaby.sh
+++ docker/run_lullaby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 /bin/prometheus_lullaby_client.py &
-/bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml &
+/bin/alertmanager --config.file=/etc/prometheus/alertmanager.yml &
 /bin/prometheus --config.file=/etc/prometheus/prometheus.yml \
                 --web.console.libraries=/etc/prometheus/console_libraries \
                 --web.console.templates=/etc/prometheus/consoles \
