0c3d8f456b51a937fd95fb1f27b040c669f0fc51
Ben Evans <benjamin.robert.evans@gmail.com>
2018-07-05 14:38:56 -0400

    actually add slack api to alertmanager config

--- docker/run_lullaby.sh
+++ docker/run_lullaby.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 python3 /bin/prometheus_lullaby_client.py &
+# drop slack secrets into alertmanager file
+sed -i 's@WEBHOOK_URL@'\'"$WEBHOOK_URL"\''@' /etc/prometheus/alertmanager.yml
 /bin/alertmanager --config.file=/etc/prometheus/alertmanager.yml &
 /bin/prometheus --config.file=/etc/prometheus/prometheus.yml \
                 --web.console.libraries=/etc/prometheus/console_libraries \
