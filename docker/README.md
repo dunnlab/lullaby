61e88de3f92ac8858770ed408d14aa0f38cb2ae8
Ben Evans <benjamin.robert.evans@gmail.com>
2018-07-12 18:37:34 +0000

    iterating on prometheus/alertmanager settings

--- docker/README.md
+++ docker/README.md
@@ -11,6 +11,17 @@ To run the container, listening on port `9090`:
 ```bash
 export PARTICLE_API_KEY="yourkeyhere"
 export WEBHOOK_URL="slackwebhookurl"
-docker run -e PARTICLE_API_KEY=${PARTICLE_API_KEY} -e WEBHOOK_URL=${WEBHOOK_URL} -p 9090:9090 lullaby
+docker run -d -e PARTICLE_API_KEY=${PARTICLE_API_KEY} -e WEBHOOK_URL=${WEBHOOK_URL} --network host --name lullaby lullaby
 
 ```
+
+To see the logs from the container:
+```bash
+docker logs lullaby
+```
+
+To run an interactive session in the container:
+```bash
+docker exec -it lullaby /bin/bash
+```
+
--- docker/run_lullaby.sh
+++ docker/run_lullaby.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python3 /bin/prometheus_lullaby_client.py &
+python3 /bin/prometheus_lullaby_exporter.py &
 # drop slack secrets into alertmanager file
-sed -i 's@WEBHOOK_URL@'\'"$WEBHOOK_URL"\''@' /etc/prometheus/alertmanager.yml
-/bin/alertmanager --config.file=/etc/prometheus/alertmanager.yml &
+sed -i 's@WEBHOOK_URL@'\'"$WEBHOOK_URL"\''@' /etc/alertmanager/alertmanager.yml
+/bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml &
 /bin/prometheus --config.file=/etc/prometheus/prometheus.yml \
                 --web.console.libraries=/etc/prometheus/console_libraries \
                 --web.console.templates=/etc/prometheus/consoles \
