3397eb2ad991a2c4837d659191b33bbe27257efd
Casey Dunn <casey.dunn@yale.edu>
2019-03-09 10:31:06 -0500

    whitespace cleanup, thermocouple temp still 0 though no faults detected

--- freezer_boron/src/freezer_boron.ino
+++ freezer_boron/src/freezer_boron.ino
@@ -25,13 +25,13 @@ Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(6, 5, 4, 3);
 
 void setup() {
 
-    Particle.variable("tempF", tempF);
-    Particle.variable("tempC", tempC);
-    Particle.variable("humidity", humidity);
+	Particle.variable("tempF", tempF);
+	Particle.variable("tempC", tempC);
+	Particle.variable("humidity", humidity);
 
-    pinMode(LED_PIN, OUTPUT);
+	pinMode(LED_PIN, OUTPUT);
 
-    Serial.begin(9600);
+	Serial.begin(9600);
 
 	dht.begin();
 
@@ -40,26 +40,26 @@ void setup() {
 
 void loop() {
 
-    // alternate the LED_PIN between high and low
-    // to show that we're still alive
-    digitalWrite(LED_PIN, (led_on_state) ? HIGH : LOW);
-    led_on_state = !led_on_state;
+	// alternate the LED_PIN between high and low
+	// to show that we're still alive
+	digitalWrite(LED_PIN, (led_on_state) ? HIGH : LOW);
+	led_on_state = !led_on_state;
 
-    // DHT22 max read time is 0.5Hz
+	// DHT22 max read time is 0.5Hz
 	delay(60000);
 
-    // Reading temperature or humidity takes about 250 milliseconds
+	// Reading temperature or humidity takes about 250 milliseconds
 	humidity = dht.getHumidity();
 	tempC = dht.getTempCelcius();
 	tempF = dht.getTempFarenheit();
 
-    // Check if any reads failed and exit early
+	// Check if any reads failed and exit early
 	if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
-		Serial.println("Failed to read from DHT sensor!");
+		Particle.publish("FAULT_DHT", "Failed to read from DHT sensor!");
 		return;
 	}
 
-    float hi = c2f(dht.getHeatIndex());
+	float hi = c2f(dht.getHeatIndex());
 	//float dp = dht.getDewPoint();
 	//float k = dht.getTempKelvin();
 
@@ -81,24 +81,24 @@ void loop() {
 
 
 	Serial.print("Cold Junction Temp: ");
-    // Serial.println(maxthermo.readCJTemperature());
-    Particle.publish("CJTemp", String(maxthermo.readCJTemperature()), PRIVATE);
+	// Serial.println(maxthermo.readCJTemperature());
+	Particle.publish("CJTemp", String(maxthermo.readCJTemperature()), PRIVATE);
 
-    Serial.print("Thermocouple Temp: ");
-    // Serial.println(maxthermo.readThermocoupleTemperature());
-    Particle.publish("ThermoTemp", String(maxthermo.readThermocoupleTemperature()), PRIVATE);
-    // Check and print any faults
-    uint8_t fault = maxthermo.readFault();
-    if (fault) {
-        if (fault & MAX31856_FAULT_CJRANGE) Serial.println("Cold Junction Range Fault");
-        if (fault & MAX31856_FAULT_TCRANGE) Serial.println("Thermocouple Range Fault");
-        if (fault & MAX31856_FAULT_CJHIGH)  Serial.println("Cold Junction High Fault");
-        if (fault & MAX31856_FAULT_CJLOW)   Serial.println("Cold Junction Low Fault");
-        if (fault & MAX31856_FAULT_TCHIGH)  Serial.println("Thermocouple High Fault");
-        if (fault & MAX31856_FAULT_TCLOW)   Serial.println("Thermocouple Low Fault");
-        if (fault & MAX31856_FAULT_OVUV)    Serial.println("Over/Under Voltage Fault");
-        if (fault & MAX31856_FAULT_OPEN)    Serial.println("Thermocouple Open Fault");
-    }
+	Serial.print("Thermocouple Temp: ");
+	// Serial.println(maxthermo.readThermocoupleTemperature());
+	Particle.publish("ThermoTemp", String(maxthermo.readThermocoupleTemperature()), PRIVATE);
+	// Check and print any faults
+	uint8_t fault = maxthermo.readFault();
+	if (fault) {
+		if (fault & MAX31856_FAULT_CJRANGE) Particle.publish("FAULT_Thermo", "Cold Junction Range Fault");
+		if (fault & MAX31856_FAULT_TCRANGE) Particle.publish("FAULT_Thermo", "Thermocouple Range Fault");
+		if (fault & MAX31856_FAULT_CJHIGH)  Particle.publish("FAULT_Thermo", "Cold Junction High Fault");
+		if (fault & MAX31856_FAULT_CJLOW)   Particle.publish("FAULT_Thermo", "Cold Junction Low Fault");
+		if (fault & MAX31856_FAULT_TCHIGH)  Particle.publish("FAULT_Thermo", "Thermocouple High Fault");
+		if (fault & MAX31856_FAULT_TCLOW)   Particle.publish("FAULT_Thermo", "Thermocouple Low Fault");
+		if (fault & MAX31856_FAULT_OVUV)    Particle.publish("FAULT_Thermo", "Over/Under Voltage Fault");
+		if (fault & MAX31856_FAULT_OPEN)    Particle.publish("FAULT_Thermo", "Thermocouple Open Fault");
+	}
 
 
 }
@@ -106,5 +106,5 @@ void loop() {
 // celcius to fahrenheit
 float c2f(float c)
 {
-    return 1.8 * c + 32;
+	return 1.8 * c + 32;
 }
